b3d8f6d0e0c373b2ae814b8e53ac88e28c3b5164
lkk-cxks <71692596+lkk-cxks@users.noreply.github.com>
2021-01-07 18:11:25 +0800

    Add files via upload

---
+++ github11/src/github11/test4.java
@@ -0,0 +1,12 @@
+package github11;
+public class test4 {
+	 final static String salutation = "Hello! ";
+	   public static void main(String args[]){
+	      GreetingService greetService1 = message -> 
+	      System.out.println(salutation + message);
+	      greetService1.sayMessage("Runoob");
+	   }
+	   interface GreetingService {
+	      void sayMessage(String message);
+	   }
+}
